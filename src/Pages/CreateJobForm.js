83a69ced85dfd1fabbd3b31d247a1a12bb81d16b
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2022-12-23 23:14:37 -0800

    managed to connect to backend by using full url route and changing backened to use cors

--- src/Pages/CreateJobForm.js
+++ src/Pages/CreateJobForm.js
@@ -1,7 +1,29 @@
+import axios from "axios"
+
 export const CreateJobForm = () => {
+    //const [value, setValue] = useState({});
+    const payload = {"firstname":"Gary","lastname":"stroup"}
+    axios.post("http://localhost:8080/dms/create/job",payload, {
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    .then((response) => response.data).then(function (data) {
+    // handle success
+        document.getElementById("first").innerHTML = data.firstname;
+        document.getElementById("last").innerHTML = data.lastname;
+        
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error.message);
+  })
     return (
         <div>
             <h1>This page will hold a form to create a new job and submit data into the database</h1>
+            <p>Firstname is: <span id="first"></span></p>
+            <p>Lastname is: <span id="last"></span></p>
+           
         </div>
     )
 }
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
 
 const Login = () => {
     return (
         <div>
             <h1>This is the login page</h1>
+            <Link to="/user/job/create">Create Job</Link>
         </div>
     )
 }
