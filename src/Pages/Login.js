2ec4dd290160ee23de689586e94ea2fb3244cde8
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2022-12-24 09:20:27 -0800

    installed tailwindcss. Created Login page and tested connection to backend.

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,12 +1,73 @@
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { useState } from "react"
+// import {Redirect} from "react-router-dom";
 
-const Login = () => {
+
+export const Login = () => {
+
+    const [username, SetUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    
+    function setValue(e) {
+        const { name, value } = e.target;
+        switch (name) {
+            case 'username':
+                SetUsername(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            default:
+                console.log(`Sorry, option not available for ${name}.`);
+        }
+    }
+
+    const LoginUser = function (e) {
+        e.preventDefault();
+        let payload = {
+            username,
+            password,
+        }
+
+        axios.post("http://localhost:8080/dms/login",payload, {
+        headers: {
+            'content-type': 'application/json'
+        }
+        })
+        .then((response) => response.data).then(function (data) {
+        // handle success
+            console.log(data)
+            document.getElementById("first").innerHTML = data.username;
+            document.getElementById("last").innerHTML = data.password;
+            
+        })
+        .catch(function (error) {
+            // handle error
+            setErrorMessage(error.response.data);
+        })
+
+        //     let message = resp.data;
+        //     if (message === "okay"){
+        //         <Redirect to="/dashboard"/>
+        //     }
+        // }
+    }
+
     return (
-        <div>
-            <h1>This is the login page</h1>
-            <Link to="/user/job/create">Create Job</Link>
+        <div className="grid grid-cols-5 grid-rows-4 gap-3 mx-auto p-2">
+            <h1 className="text-purple-700 text-xl col-span-5 mx-auto py-4 md:text-6xl">Department Management System</h1>
+            <form className="grid shadow-xl p-4 row-start-2 row-span-2 col-start-2 col-span-3">
+                <label className="block mt-3" htmlFor="username">User Name</label>
+                <input className="shadow-md my-3 rounded-md px-4 py-3" type="text" id="username" name="username" value={username} onChange={setValue} />
+                <label className="block" htmlFor="password">Password</label>
+                <input className="shadow-md my-3 px-4 py-3 rounded-md mb-4" type="text" id="password" name="password" value={password} onChange={setValue} />
+                <hr />
+                <input className="text-white font-bold bg-purple-400 rounded-lg p-4 my-3" type="submit" value="Login" onClick={LoginUser} />
+            </form>
+            <h1 className="text-red-500 md:text-2xl row-start-4 col-start-2 col-span-2 mt-4">{errorMessage}</h1>
+            <p id="first"></p>
+            <p id="last"></p>
         </div>
     )
 }
-
-export {Login}
