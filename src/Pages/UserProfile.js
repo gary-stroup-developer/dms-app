1a410c940db7e19b034f834c3014102144fade8c
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-01-01 00:38:49 -0800

    successfully load data from backend to user profile. need to work on create job and read job in the modal functionality

--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -12,40 +12,62 @@ export const UserProfile = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const [queue, setQueue] = useState([]);
+    const [wip, SetWip] = useState([]);
+    const [staged, SetStaged] = useState([]);
+    const [complete, SetComplete] = useState(0);
+
+    const [first, SetFirst] = useState("");
+    const [last, SetLast] = useState("");
+    const [capacity, setCapacity] = useState(0);
+
     const [id, setID] = useState("");
 
     const [errorMessage, setErrorMessage] = useState("");
 
     let userID = useParams();
-
+    
     useEffect(() => {
         setID(userID.id)
-        axios.post("http://localhost:8080/dms/userprofile/jobs", {uid:"X202202"}, {
+        axios.post("http://localhost:8080/dms/userprofile/jobs", {uid:id}, {
             headers: {
                 'content-type': 'application/json'
             }
         }).then((response) => {
-            console.log(response.data)
-            //set the role for the current user logged in
-            
-
-            // let respUser = []; //hold the array of <Card />
-            // let totalCapacity = 0;
-
-            // for (let user of response.data) {
-                // totalCapacity += (user.capacity / 6.5);
-                
-                // respUser.push(<Card key={user._id} capacity={((user.capacity / 6.5) * 100).toFixed(2)} fname={user.firstname} lname={user.lastname} id={user.uid} />)
-                
-                // setCapacity(totalCapacity/(response.data.length))
-                // setUserProfile(respUser.map(u => u))
-            // }
+            let user = response.data.Users[0];
+            let jobs = response.data.Jobs[0];
+            if (user) {
+                SetFirst(user.firstname);
+                SetLast(user.lastname);
+                setCapacity(((user.capacity/6.5)*100).toFixed(2))
+            }
+            if (jobs) {
+                switch (jobs._id) {
+                    case "queue":
+                        setQueue(jobs.jobs.map((record) => record));
+                        break;
+                    case "wip":
+                        SetWip(jobs.jobs.map((record) => record));
+                        break;
+                    case "staged":
+                        SetStaged(jobs.jobs.map((record) => record));
+                        break;
+                    case "complete":
+                        SetComplete(jobs.jobs.length);
+                        break;
+                    default:
+                        setErrorMessage("Jobs unavailable at this time");
+                }
+            }
             })
-            .catch(function (error) {
+            .catch((error) => {
                 // handle error
-                setErrorMessage(error.response.data);
+                while (!error) {
+                    console.log("uh oh")
+                }
+                console.log(error)
             });
-    },[]);
+    },[id,userID]);
 
     function toggleContent(section) {
         let content = document.getElementById(section);
@@ -58,47 +80,52 @@ export const UserProfile = () => {
         <div>
             {errorMessage}
             <div className="text-slate-700 border-b border-black grid grid-cols-3 grid-rows-3 p-4">
-                <h1 className="text-3xl row-start-1 col-start-2 col-span-2 mb-4 md:text-5xl">Gary Stroup</h1>
+                <h1 className="text-3xl row-start-1 col-start-2 col-span-2 mb-4 md:text-5xl">{first} { last}</h1>
                 <Link className="row-start-2 self-center justify-self-center p-4 md:text-2xl hover:text-purple-600" to="/dashboard/admin">Dashboard</Link>
                 {/* <Link className="row-start-3 md:text-2xl" to="/dashboard/user">Create Job</Link> */}
                 <Button variant="link" className="row-start-3 text-slate-700 md:text-2xl border-none no-underline hover:text-purple-600" onClick={handleShow}>
                     Create Job
                 </Button>
-                <p className="row-start-2 col-start-3 self-center justify-self-center p-4 md:text-2xl">Total Jobs: 54</p>
-                <p className="row-start-3 col-start-3 self-center justify-self-center p-4 md:text-2xl">Capacity: 88% </p>
+                <p className="row-start-2 col-start-3 self-center justify-self-center p-4 md:text-2xl">Total Jobs: {complete }</p>
+                <p className="row-start-3 col-start-3 self-center justify-self-center p-4 md:text-2xl">Capacity: {capacity}% </p>
             </div>
             <div className="flex flex-wrap mt-5 justify-around">
                 <div className="flex-col basis-10/12 mt-2 md:basis-1/4">
-                    <h1 className="text-white bg-purple-600 p-3 rounded" onClick={()=>toggleContent("queue")}>Queue</h1>
+                    <div className="flex justify-between text-white bg-purple-600 p-3 rounded" onClick={()=>toggleContent("queue")}>
+                        <h1>Queue</h1>
+                        <p>{queue.length}</p>
+                    </div>
+                    
                     <div id="queue">
-                        <JobCard key={1} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={2} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={3} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
+                        {
+                            queue.map(job =><JobCard key={job._id} desc={job.raw_desc} pn={job.cat_num} ln={job.cat_lot} jobid={job._id} due={"2022-12-31"}/>)
+                        }
                     </div>
                 </div>
+
                 <div className="flex-col basis-10/12 mt-2 md:basis-1/4">
-                    <h1 className="text-white bg-purple-600 p-3 rounded" onClick={()=>toggleContent("wip")}>WIP</h1>
+                    <div className="flex justify-between text-white bg-purple-600 p-3 rounded" onClick={()=>toggleContent("wip")}>
+                        <h1>WIP</h1>
+                        <p>{wip.length}</p>
+                    </div>
+
                     <div id="wip">
-                        <JobCard key={4} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={5} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={6} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={7} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={8} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={9} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={10} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={11} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={12} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={13} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={14} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={15} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
+                        {
+                            wip.map(job =><JobCard key={job._id} desc={job.raw_desc} pn={job.cat_num} ln={job.cat_lot} jobid={job._id} due={"2022-12-31"}/>)
+                        }
                     </div>
                 </div>
+
                 <div className="flex-col basis-10/12 mt-2 md:basis-1/4">
-                    <h1 className="text-white bg-purple-600 p-3 rounded" onClick={()=>toggleContent("staged")}>Staged</h1>
+                    <div className="flex justify-between text-white bg-purple-600 p-3 rounded" onClick={() => toggleContent("staged")}>
+                        <h1>Staged</h1>
+                        <p>{staged.length}</p>
+                    </div>
+                    
                     <div id="staged">
-                        <JobCard key={16} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={23} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
-                        <JobCard key={55} desc={"Stem Cells"} pn={"sc-456771"} ln={"3145567"} jobid={"mgd-2022-1jdye3"} due={"2022-12-31"}/>
+                        {
+                            staged.map(job =><JobCard key={job._id} desc={job.raw_desc} pn={job.cat_num} ln={job.cat_lot} jobid={job._id} due={"2022-12-31"}/>)
+                        }
                     </div>
                 </div>
             </div>
